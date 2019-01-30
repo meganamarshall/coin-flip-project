cd75b7463e0c0bcae50d2907f5a6722bd279430e
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-30 14:29:01 -0800

    final steps

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import catOrDog from './cat-or-dog.js';
 
 const pickForm = document.getElementById('pick-form');
 const image = document.getElementById('animal-pic');
+const dogNode = document.getElementById('dog-count');
+const catNode = document.getElementById('cat-count');
+
+let catCount = 0;
+let dogCount = 0;
 
 pickForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -12,9 +17,13 @@ pickForm.addEventListener('submit', function(event) {
 
     if(animal === 'cat') {
         imageSource = '../assets/meancat.jpeg';
+        catCount++;
+        catNode.textContent = catCount;
     }
     else {
         imageSource = '../assets/nicedog.jpeg';
+        dogCount++;
+        dogNode.textContent = dogCount;
     }
 
     image.src = imageSource;
